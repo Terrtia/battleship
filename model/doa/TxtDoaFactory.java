c388e288bec8dcfc2e64be0473e7c28a18c3fe22
Terrtia <or1994@hotmail.fr>
2017-05-02 16:57:42 +0200

    javadoc DOA

--- model/doa/TxtDoaFactory.java
+++ model/doa/TxtDoaFactory.java
@@ -1,12 +1,21 @@
 // TXT DOA Factory implementation
 package battleship.model.doa;
 
+/**
+ * 
+ * this class create the correct Txt DOA
+ *
+ */
 public class TxtDoaFactory extends AbstractDoaFactory {
 
 	public TxtDoaFactory() {
 		
 	}
 
+	/**
+	 * create a DoaNormalGame for a normal game with Txt DOA
+	 * @return DoaNormalGame ( TxtNormalGameFactory, ... )
+	 */
 	@Override
 	public DoaNormalGame getNormalGameTxtDoa() {
 		//TxtDoaFactory implement DoaNormalGame
