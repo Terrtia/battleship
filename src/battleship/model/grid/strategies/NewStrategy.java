c2b3b6ef1b7a610c2ad4f36d1164478c93f5802d
NGauville <noreo.ercoli@gmail.com>
2017-05-17 15:46:41 +0200

    nouvelle stratégie

--- src/battleship/model/grid/strategies/NewStrategy.java
+++ src/battleship/model/grid/strategies/NewStrategy.java
@@ -4,13 +4,21 @@ import battleship.model.grid.Grid;
 import battleship.model.grid.Grid.Square;
 
 public class NewStrategy extends Strategy {
-
+	private int x = -1;
+	private int y = 0;
+	
 	@Override
 	public void play(Grid g) {
-		int x = -1, y = -1;
-		do{
-			x = rng.nextInt(g.getGridSize());
-			y = rng.nextInt(g.getGridSize());
-		} while(g.getSquare(x, y) != Square.EMPTY);
+		x++;
+		if(x >= g.getGridSize()) {
+			x = 0;
+			y++;
+		}
+		
+		if(y >= g.getGridSize()) {
+			y = 0;
+		}
+		
+		g.isHit(x, y);
 	}
 }
